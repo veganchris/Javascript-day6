b2bf8b9572d5e55de87eb7955602a01f5585baae
Chris Runda <vegan@gmx.at>
2023-10-05 13:35:36 +0200

    Day6 files added

---
+++ books.js
@@ -0,0 +1,38 @@
+let allBooks = `
+[{
+"title" : "A Game of Thrones",
+"author" : "George R.R. Martin",
+"cover" : "https://images.thalia.media/00/-/d9689426a27e46e2bb22952fd23b9c7f/a-game-of-thrones-gebundene-ausgabe-george-r-r-martin-englisch.jpeg",
+"read" : "true"
+},{
+"title" : "Making It So",
+"author" : "Patrick Stewart",
+"cover" : "https://images.thalia.media/00/-/458ddc826a0149599fd557b9719c6f02/making-it-so-gebundene-ausgabe-patrick-stewart-englisch.jpeg",
+"read" : "false"
+},{
+"title" : "Steve Jobs",
+"author" : "Walter Isaacson",
+"cover" : "https://images.thalia.media/00/-/9f395f51b3cb4c1ca787ec6dd6ebd74d/steve-jobs-gebundene-ausgabe-walter-isaacson-englisch.jpeg",
+"read" : "false"
+},{
+"title" : "Start Where You are",
+"author" : "Meera Lee Patel",
+"cover" : "https://images.thalia.media/00/-/159b2c2946f142beaae3d42e3f3702df/start-where-you-are-taschenbuch-meera-lee-patel-englisch.jpeg",
+"read" :"true"
+},{
+"title" : "Good Omens",
+"author" : "Neil Gaiman",
+"cover" : "https://images.thalia.media/00/-/e2573efbf3984be3bda7086359913484/the-quite-nice-and-fairly-accurate-good-omens-script-book-taschenbuch-neil-gaiman-englisch.jpeg",
+"read" : "false"
+},{
+"title" : "Finding Your Element",
+"author" : "Ken Robinson",
+"cover" : "https://images.thalia.media/00/-/9d8aa6aa18a743c8b3fb6e1ffd11ca98/finding-your-element-how-to-discover-your-talents-and-passions-and-transform-your-life-gebundene-ausgabe-ken-robinson-englisch.jpeg",
+"read" : "true"
+},{
+"title" : "Cleat Cute",
+"author" : "Meryl Wilsner",
+"cover" : "https://images.thalia.media/00/-/91b1bbcfadda4c9fbc640ee474e95e00/cleat-cute-taschenbuch-meryl-wilsner-englisch.jpeg",
+"read" : "false"
+}]
+`
---
+++ employees.js
@@ -0,0 +1,73 @@
+let employees = `
+[{
+"UniquID" : "1",
+"FirstName" : "Chris",
+"LastName" : "Runda",
+"Emailaddress" : "vegan@gmx.at",
+"JobTitle" : "Entrepreneur",
+"Salary" : "0"
+},{
+"UniquID" : "2",
+"FirstName" : "Max",
+"LastName" : "Muster",
+"Emailaddress" : "max@gmx.at",
+"JobTitle" : "Shopper",
+"Salary" : "200"
+},{
+"UniquID" : "3",
+"FirstName" : "Karl",
+"LastName" : "Ermou",
+"Emailaddress" : "k_e@gmx.at",
+"JobTitle" : "Restaurant Owner",
+"Salary" : "5000"
+},{
+    "UniquID" :"4",
+"FirstName" : "Franz",
+"LastName" : "Lurch",
+"Emailaddress" : "lurch_f@gmx.at",
+"JobTitle" : "Pilot",
+"Salary" : "8000"
+},{
+    "UniquID" : "5",
+"FirstName" : "Rudi",
+"LastName" : "Ratslos",
+"Emailaddress" : "loser@gmx.at",
+"JobTitle" : "Underdog",
+"Salary" : "400"
+},{
+    "UniquID" : "6",
+"FirstName" : "Julia",
+"LastName" : "Star",
+"Emailaddress" : "stars@gmx.at",
+"JobTitle" : "Actor",
+"Salary" : "100000"
+},{
+    "UniquID" : "7",
+"FirstName" : "Sigrid",
+"LastName" : "Cinnamon",
+"Emailaddress" : "spice@gmx.at",
+"JobTitle" : "Farmer",
+"Salary" : "1000"
+},{
+    "UniquID" : "8",
+"FirstName" : "Mary",
+"LastName" : "Holy",
+"Emailaddress" : "saint@gmx.at",
+"JobTitle" : "Virgin",
+"Salary" : "9999999"
+},{
+    "UniquID" : "9",
+"FirstName" : "Heidi",
+"LastName" : "Master",
+"Emailaddress" : "master@chess.at",
+"JobTitle" : "Chessplayer",
+"Salary" : "50000"
+},{
+    "UniquID" : "10",
+"FirstName" : "Lydia",
+"LastName" : "Conner",
+"Emailaddress" : "lcon@gmx.at",
+"JobTitle" : "Programmer",
+"Salary" : "3000"
+}]
+`
